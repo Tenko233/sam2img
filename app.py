6b1cee0f1b4c0c4b69fad4bc68f0d80e8a8c49db
汪天 <1282057552@qq.com>
2023-05-09 21:21:00 +0800

    updated functions

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
+from PIL import Image
+import numpy as np
+
 from sam_tools import *
 from image_tools import *
 
-# Set parameters here
+# set parameters here
 input_image = "input/1.jpg"
 output_folder = "output"
 model = "vit_h"  # "vit_h", "vit_b" or "vit_l"
-segmentation_method = "point"  # "point", "box" or "auto"
+segmentation_method = "point"  # "point", "box", "auto", "all"
 multi_output_mask = True  # only available for "point" or "box" mode
 output_layer = "cut"  # "cut", "mask only" or "both"
+
+input_points = [[700, 50], [2000, 1000]]  # coordinates of point prompts, only available for "point" mode
+input_labels = [0, 1]  # 0 for in the ROI and 1 for out,only available for "point" mode
+
+input_box = [100, 500, 1000, 1500]  # coordinates of the diagonal vertexes of the box, only available for "box" mode
+# end of parameters
+
+# load image and check whether the points and box are inside the image
+image = Image.open(input_image)
+size = image.size
+
+if input_points:
+    for point in input_points:
+        if not is_in_image(point, size):
+            raise Exception("Point coordinates are not in the image.")
+if input_box:
+    vertexes = [[input_box[0], input_box[1]], [input_box[2], input_box[3]]]
+    for vertex in vertexes:
+        if not is_in_image(vertex, size):
+            raise Exception("Box coordinates are not in the image.")
+
+# do segmentation task and generate layers
+psd = create_psd_from_image(image)
+
+if segmentation_method == "point" or "all":
+    masks, _, _ = contour_with_points(image, input_points, input_labels, model, multi_output_mask)
+    for mask in masks:
+        layer, mask = mask_to_layer(image, mask, output_layer)
+        psd = add_layer_and_mask(psd, layer, mask)
+
--- image_tools.py
+++ image_tools.py
@@ -1,1 +1,56 @@
-import psd_tools
+from psd_tools import PSDImage
+from psd_tools.api.mask import Mask
+import numpy as np
+from PIL import Image
+
+
+def create_psd_from_image(image):
+    psd = PSDImage.new(image.size[0], image.size[1])
+    psd.add_layer(image)
+    return psd
+
+
+def is_in_image(point, size):
+    w = size[0]
+    h = size[1]
+    is_in = True
+    if point[0] < 0 or point[0] > w or point[1] < 0 or point[1] > h:
+        is_in = False
+
+    return is_in
+
+
+def mask_to_layer(image, mask, mode):
+    if mode == "cut":
+        array = np.array(image)
+        print(array.shape)
+        mask = mask.reshape(1, mask.shape[0], mask.shape[1])
+        layer = array * mask
+        layer = Image.fromarray(layer)
+        out_mask = None
+
+    elif mode == "mask only":
+        mask = (mask * 255).astype(np.uint8)
+        layer = Image.fromarray(mask)
+        out_mask = None
+
+    elif mode == "both":
+        layer = image
+        mask = (mask * 255).astype(np.uint8)
+        out_mask = Image.fromarray(mask)
+    else:
+        print("Invalid mode.")
+        layer = None
+        out_mask = None
+
+    return layer, out_mask
+
+
+def add_layer_and_mask(psd: PSDImage, layer:Image, mask: Image):
+    if layer is not None:
+        source_layer = psd.add_layer(layer)
+        if mask is not None:
+            mask = Mask(mask.width, mask.height, mask.tobytes())
+            source_layer.mask = mask
+
+    return psd
